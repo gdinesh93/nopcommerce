f078b79be5e4c00c6bc45bc5d27a4198b3781fd4
gdinesh93 <gdineshmurugan@gmail.com>
2023-02-05 22:50:03 +0530

    second commit

--- testcases/test.py
+++ testcases/test.py
@@ -1,10 +1,11 @@
-def test_prime():
+def test_prime(n):
   count=0
+
   for i in range(2,n):
     if i%n==0:
         count+=1
   if count==2:
-    print("the number is not prime)
+    print("the number is not prime")
   else:
-          print("the number is prime")
+    print("the number is prime")
      
