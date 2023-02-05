11bfac9e6ee957c8eb5d8a931e64dfe916be7e87
gdinesh93 <108114979+gdinesh93@users.noreply.github.com>
2023-02-05 22:45:37 +0530

    Create test.py

---
+++ testcases/test.py
@@ -0,0 +1,10 @@
+def test_prime():
+  count=0
+  for i in range(2,n):
+    if i%n==0:
+        count+=1
+  if count==2:
+    print("the number is not prime)
+  else:
+          print("the number is prime")
+     
